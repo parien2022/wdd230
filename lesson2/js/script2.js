9e57744e8fd402e3039df578a145426e9329ddf3
parien2022 <par22002@byui.edu>
2022-05-07 20:42:44 -0300

    Responsive Desing

---
+++ lesson2/js/script2.js
@@ -0,0 +1,5 @@
+const d = new Date();
+const year = d.getFullYear();
+
+document.getElementById("update_info").textContent = document.lastModified;
+document.getElementById("year").textContent = year;
