59b082d4f0b867cfec36c678392a10abee7c3ac1
parien2022 <par22002@byui.edu>
2023-06-03 21:09:40 -0300

    New info added for each section

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -23,3 +23,17 @@ document.querySelector('#date').textContent = formattedDate
 
 const lastModified = document.lastModified
 document.querySelector("#LastModification").textContent = lastModified
+
+
+const joinUs = document.querySelector("#join-us");
+
+
+const dayOptions = {
+    weekday: 'long',
+}
+const day = date.toLocaleDateString('en-US', dayOptions);
+
+if (day == 'Monday' || day == 'Tuesday'){
+    console.log("success")
+    joinUs.style.display = "block";
+}
